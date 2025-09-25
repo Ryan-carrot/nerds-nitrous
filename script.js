8ee1dba9786af615aa5bb0a26df0838032593af0
Ryan Christian <Ryan@carrot.com>
2025-09-25 16:52:17 -0700

    Add Mobile Responsiveness
    Added:
    -Hamburger menu for screens under 777px
    -Swapped logo on screens under 777px
    -Stacked gallery and collab section under 500px
    -Slightly adjusted hamburger menus margin for really small screens (under 375px)

---
+++ script.js
@@ -0,0 +1,5 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("nav-links");
+  x.classList.toggle("show");
+}
