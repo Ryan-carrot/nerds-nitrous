87117ba4ccf1882febcc577be583c3a0fd2e2ba2
Ryan Christian <Ryan@carrot.com>
2025-09-27 13:45:40 -0700

    Add Animations
    Added:
    -Keyframes for .welcome h2 slide in
    -Keyframes for gallery-grid fade in
    -jQuery to trigger fade in on scroll

--- script.js
+++ script.js
@@ -3,3 +3,26 @@ function myFunction() {
   var x = document.getElementById("nav-links");
   x.classList.toggle("show");
 }
+
+// jQuery to check scroll position to trigger animation
+document.addEventListener("DOMContentLoaded", function(event) {
+    document.addEventListener("scroll", function(event) {
+        const galleryGrids = document.getElementsByClassName("gallery-grid");
+        const windowOffsetTop = window.innerHeight + window.scrollY;
+
+        Array.prototype.forEach.call(galleryGrids, (galleryGrid) => {
+            const galleryGridOffsetTop = galleryGrid.offsetTop;
+
+            if (windowOffsetTop >= galleryGridOffsetTop) {
+                addClass(galleryGrid, "slide-in-fwd-center");
+            }
+        });
+    });
+});
+
+function addClass(element, className) {
+    const arrayClasses = element.className.split(" ");
+    if (arrayClasses.indexOf(className) === -1) {
+        element.className += " " + className;
+    }
+}
